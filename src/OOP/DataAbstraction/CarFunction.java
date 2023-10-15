15e2c39bd70f5e1dd7da1f1644aafd2653dd0bfd
Janhavi <janhavidahatonde@gmail.com>
2023-10-15 08:33:59 +0530

    Day 2 - Java OOP Concept

---
+++ src/OOP/DataAbstraction/CarFunction.java
@@ -0,0 +1,27 @@
+package OOP.DataAbstraction;
+
+abstract class CarFun {
+    abstract void move();
+
+    abstract void stop();
+}
+
+class CarFunImpl extends CarFun {
+    @Override
+    void move() {
+        System.out.println("Car is moving");
+    }
+
+    @Override
+    void stop() {
+        System.out.println("Car is stopping");
+    }
+}
+
+public class CarFunction {
+    public static void main(String[] args) {
+        CarFunImpl c1 = new CarFunImpl();
+        c1.move();
+        c1.stop();
+    }
+}
---
+++ src/OOP/Inheritance/ChildImpl.java
@@ -0,0 +1,30 @@
+package OOP.Inheritance;
+
+interface Mother {
+    void printMother();
+}
+
+interface Father {
+    void printFather();
+}
+
+class Child implements Mother, Father {
+
+    @Override
+    public void printMother() {
+        System.out.println("In mother interface");
+    }
+
+    @Override
+    public void printFather() {
+        System.out.println("In father interface");
+    }
+}
+
+public class ChildImpl {
+    public static void main(String[] args) {
+        Child child = new Child();
+        child.printMother();
+        child.printFather();
+    }
+}
