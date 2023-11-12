989e2147e1a45d893b9ef219bda1cd243fba3ce7
PixelPioneers0 <pixelpioneers2023.official@gmail.com>
2023-11-12 11:10:53 +0530

    Diwali Pattern

--- src/DiwaliPattern.java
+++ src/DiwaliPattern.java
@@ -1,5 +1,18 @@
 public class DiwaliPattern {
     public static void main(String[] args) {
+
+//            *
+//          * * *
+//        * * * * *
+//          * * *
+//            *
+//            * * * * * * * * * * * * * * * * *
+//             * * * * * * * * * * * * * * * *
+//              * * * * * * * * * * * * * * *
+//               * * * * * * * * * * * * * *
+//                * * * * * * * * * * * * *
+//                 * * * * * * * * * * * *
+
         for (int i = 1; i < 3; i++) {
             for (int j = 0; j < 3 - i; j++) {
                 System.out.print("  ");
